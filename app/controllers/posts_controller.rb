655016f2c2742d4de6e10526cbe852cdc24ed9c2
open64 <systemsofdown@gmail.com>
2014-01-14 15:14:26 +0200

    add Post controller and model

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,16 @@
+class PostsController < ApplicationController
+  def new
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  def delete
+  end
+
+  def edit
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 AdminBlog::Application.routes.draw do
+  get "posts/new"
+  get "posts/index"
+  get "posts/show"
+  get "posts/delete"
+  get "posts/edit"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
