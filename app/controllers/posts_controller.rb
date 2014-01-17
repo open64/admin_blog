2a8e6abb154c73b522fc8804fc20e7703f7d3604
open64 <systemsofdown@gmail.com>
2014-01-17 16:27:43 +0200

    add admin_active and ckeditor

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,16 +1,40 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
   def new
+    @post = Post.new
   end
 
   def index
+    @posts = Post.all
   end
 
   def show
   end
 
-  def delete
+  def destroy
+    @post.destroy
+    redirect_to root_path
   end
 
   def edit
+  end
+
+  def create
+    @post = Post.new post_params
+    @post.save
+    redirect_to root_path
+  end
+
+  def update
+    @post.update_attribute post_params
+  end
+
+  private
+  def set_post
+    @post = Post.find params[:id]
+  end
+
+  def post_params
+    params.require(:post).permit( :title, :description )
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,18 @@
 AdminBlog::Application.routes.draw do
+  mount Ckeditor::Engine => '/ckeditor'
+  root 'posts#index'
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  get "posts/new"
-  get "posts/index"
-  get "posts/show"
-  get "posts/delete"
-  get "posts/edit"
+  resources :posts
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
 
   # Example resource route with options:
   #   resources :products do
